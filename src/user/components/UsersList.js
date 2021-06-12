9234f97234709151871ad8ffd21f03d6ad8a6cfb
dariokl <darioklopic@gmail.com>
2021-06-12 14:18:38 +0200

    created users list component

---
+++ src/user/components/UsersList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
+import "./UsersList.css";
+
+const UsersList = (props) => {
+  {
+    props.lenght ? return <span>aaaa</span> : <span>bbb</span>;
+  }
+};
+
+export default UsersList;
