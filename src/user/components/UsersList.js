7b3556c7a1023ce2fb0db1e1e007b08a157e444a
dariokl <darioklopic@gmail.com>
2021-06-14 15:28:17 +0200

    changes on side drawer

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -5,9 +5,29 @@ import Card from "../../shared/components/UIElements/Card";
 import "./UsersList.css";
 
 const UsersList = (props) => {
-  {
-    props.lenght ? return <span>aaaa</span> : <span>bbb</span>;
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
+      </div>
+    );
   }
+
+  return (
+    <ul className="users-list">
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default UsersList;
