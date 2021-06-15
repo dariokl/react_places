deb4eda449f482aa26bbc115313c3198fdc817d7
dariokl <darioklopic@gmail.com>
2021-06-15 13:21:20 +0200

    map modal added

---
+++ src/places/components/PlaceList.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+import PlaceItem from "./PlaceItem";
+import "./PlaceList.css";
+
+const PlaceList = (props) => {
+  if (props.items.length === 0) {
+    console.log("a");
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2> No places found. Maybe create one ?</h2>
+          <Button>Share Place</Button>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="place-list">
+      {props.items.map((place) => (
+        <PlaceItem
+          key={place.id}
+          id={place.id}
+          image={place.imageUrl}
+          title={place.title}
+          descritpion={place.description}
+          address={place.address}
+          creatorId={place.creator}
+          coordinates={place.location}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default PlaceList;
